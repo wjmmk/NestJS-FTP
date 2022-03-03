7ff0c2f742697962a0815c5b112cb909a92fac90
William Mosquera <wjmmk3@gmail.com>
2022-03-02 23:43:08 -0500

    Basic Test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { FtpModule } from 'nestjs-ftp';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    FtpModule.forRootFtpAsync({
+      useFactory : async ()=>{
+          return {
+            host:"test.rebex.net",
+            password:"password",
+            port:21,
+            user:"demo",
+            secure:true
+          }
+      },
+      inject: [AppService]
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { FtpService } from 'nestjs-ftp';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly _ftpService: FtpService){}
+
+  async uploadFile(): Promise<string> {
+      try {
+          await this._ftpService.upload(source,destination)
+      } catch (error) {
+          throw new Error(error)
+      }
+      return "Cargado!!!";
   }
 }
